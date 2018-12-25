d54285752d3354591cd12308a9783ee52c433cae
fanbanban <18261446150@163.com>
2018-12-25 15:54:08 +0800

    简单状态机

---
+++ StateMachine/Process.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    public enum ProcessState
+    {
+        Inactive,
+        Active,
+        Paused,
+        Terminated
+    }
+
+    public enum Command
+    {
+        Begin,
+        End,
+        Pause,
+        Resume,
+        Exit
+    }
+
+    public class Process
+    {
+        class StateTransition
+        {
+            readonly ProcessState CurrentState;
+            readonly Command Command;
+
+            public StateTransition(ProcessState currentState, Command command)
+            {
+                CurrentState = currentState;
+                Command = command;
+            }
+
+            public override int GetHashCode()
+            {
+                return 17 + 31 * CurrentState.GetHashCode() + 31 * Command.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                StateTransition other = obj as StateTransition;
+                return other != null && this.CurrentState == other.CurrentState && this.Command == other.Command;
+            }
+        }
+
+        Dictionary<StateTransition, ProcessState> transitions;
+        public ProcessState CurrentState { get; private set; }
+
+        public Process()
+        {
+            CurrentState = ProcessState.Inactive;
+            transitions = new Dictionary<StateTransition, ProcessState>
+            {
+                { new StateTransition(ProcessState.Inactive, Command.Exit), ProcessState.Terminated },
+                { new StateTransition(ProcessState.Inactive, Command.Begin), ProcessState.Active },
+                { new StateTransition(ProcessState.Active, Command.End), ProcessState.Inactive },
+                { new StateTransition(ProcessState.Active, Command.Pause), ProcessState.Paused },
+                { new StateTransition(ProcessState.Paused, Command.End), ProcessState.Inactive },
+                { new StateTransition(ProcessState.Paused, Command.Resume), ProcessState.Active }
+            };
+        }
+
+        public ProcessState GetNext(Command command, StackTrace stackTrace, object target)
+        {
+            var className = target.GetType().Name;
+            var methodName = stackTrace.GetFrame(0).GetMethod().Name;
+            StateTransition transition = new StateTransition(CurrentState, command);
+            ProcessState nextState;
+            if (!transitions.TryGetValue(transition, out nextState))
+                throw new Exception("Invalid transition: " + CurrentState + " -> " + command);
+            return nextState;
+        }
+
+        public ProcessState MoveNext(Command command, StackTrace stackTrace, object target)
+        {
+
+            CurrentState = GetNext(command, stackTrace, target);
+            return CurrentState;
+        }
+    }
+}
---
+++ StateMachine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StateMachine
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+            Console.WriteLine("Current State = " + p.CurrentState);
+            Console.WriteLine("Command.Begin: Current State = " + p.MoveNext(Command.Begin, new System.Diagnostics.StackTrace(), p));
+            //Console.WriteLine("Command.Pause: Current State = " + p.MoveNext(Command.Pause));
+            //Console.WriteLine("Command.End: Current State = " + p.MoveNext(Command.End));
+            //Console.WriteLine("Command.Exit: Current State = " + p.MoveNext(Command.Exit));
+            Console.ReadLine();
+        }
+    }
+}
